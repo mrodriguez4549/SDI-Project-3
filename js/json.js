e9a4e26cdfea61f782c90c1cd62333a129bf7c97
Illastr8 <Illastr8@Illastr8s-MacBook-Pro.local>
2012-05-17 17:02:16 -0500

    Added my JSON file to JS folder.

---
+++ js/json.js
@@ -0,0 +1,90 @@
+// Author: Marco Rodriguez
+// json objects
+// cars performance numers
+
+var json = {
+	"testing": {
+		"pass1": {
+			"pass": 1,
+			"boost": 5,
+			"sixtyft": 1.55,
+			"eightmile": 6.21,
+			"quartermile": 10.05,
+			"mph": 125			
+		},
+		"pass2": {
+			"pass": 2,
+			"boost": 8,
+			"sixtyft": 1.51,
+			"eightmile": 6.18,
+			"quartermile": 9.98,
+			"mph": 127
+		},
+		"pass3": {
+			"pass": 3,
+			"boost": 12,
+			"sixtyft": 1.48,
+			"eightmile": 6.13,
+			"quartermile": 9.75,
+			"mph": 131
+		},
+		"pass4": {
+			"pass": 4,
+			"boost": 15,
+			"sixtyft": 1.42,
+			"eightmile": 6.09,
+			"quartermile": 9.55,
+			"mph": 135
+		},
+		"pass5": {
+			"pass": 5,
+			"boost": 18,
+			"sixtyft": 1.39,
+			"eightmile": 6.01,
+			"quartermile": 9.33,
+			"mph": 137
+		},
+		"pass6": {
+			"pass": 6,
+			"boost": 20,
+			"sixtyft": 1.35,
+			"eightmile": 5.99,
+			"quartermile": 9.28,
+			"mph": 140
+		},
+		"pass7": {
+			"pass": 7,
+			"boost": 22,
+			"sixtyft": 1.30,
+			"eightmile": 5.79,
+			"quartermile": 8.99,
+			"mph": 150
+		},
+		"pass8": {
+			"pass": 8,
+			"boost": 23,
+			"sixtyft": 1.29,
+			"eightmile": 5.71,
+			"quartermile": 8.74,
+			"mph": 165
+		},
+		"pass9": {
+			"pass": 9,
+			"boost": 25,
+			"sixtyft": 1.24,
+			"eightmile": 4.99,
+			"quartermile": 8.35,
+			"mph": 176
+		},
+		"pass10": {
+			"pass": 10,
+			"boost": 30,
+			"sixtyft": 1.21,
+			"eightmile": 4.90,
+			"quartermile": 8.21,
+			"mph": 185
+		}
+
+	}
+};
+
